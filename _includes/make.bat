b3aacbc38017b2952b2a4c0f131b139f51d2dee7
belongwqz <287117900@qq.com>
2014-10-25 15:22:05 +0800

    Create make.bat

---
+++ _includes/make.bat
@@ -0,0 +1,2 @@
+rem ocra CompareTool.rb --debug --chdir-first --no-autoload --no-enc --icon res.ico --gem-minimal --gem-extras res.ico --output Tool.exe
+ocra CompareTool.rbw --no-autoload --no-enc --icon res.ico --gem-minimal --gem-extras res.ico --output 工具.exe
