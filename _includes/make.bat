64c6ee59f60f305379b9d30eac151d9dd7af2615
belongwqz <287117900@qq.com>
2014-11-21 10:11:40 +0800

    Update make.bat

--- _includes/make.bat
+++ _includes/make.bat
@@ -1,2 +1,3 @@
-rem ocra CompareTool.rb --debug --chdir-first --no-autoload --no-enc --icon res.ico --gem-minimal --gem-extras res.ico --output Tool.exe
-ocra CompareTool.rbw --no-autoload --no-enc --icon res.ico --gem-minimal --gem-extras res.ico --output 工具.exe
+rem ocra CompareTool.rbw --debug --chdir-first --no-autoload --no-enc --icon res.ico --gem-minimal --gem-extras res.ico --output CompareTool.exe
+rem ocra CompareTool.rbw --no-autoload --no-enc --icon res.ico --gem-minimal --gem-extras res.ico --output 配置文件比对工具.exe
+ocra CompareTool.rbw --no-lzma --chdir-first --no-autoload --no-enc --icon res.ico --gem-minimal --gem-extras res.ico --innosetup setup.iss --output 配置文件比对工具.exe
